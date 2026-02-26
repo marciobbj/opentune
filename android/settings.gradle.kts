69b482ae803251e6a1e5b5f34bca1be31c2fcc25
Marcio Bernardes <marciobernardes@live.com>
2026-02-25 21:16:31 -0300

    fix merge

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -8,7 +8,8 @@ pluginManagement {
             flutterSdkPath
         }
 
-    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+    includeBuild("flutter_tools_gradle")
+
 
     repositories {
         google()
